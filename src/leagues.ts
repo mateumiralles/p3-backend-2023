d5279371c4776be949ab86cca2277bf2bb0aee68
MateuMiralles <mateumiralles714@gmail.com>
2023-06-05 07:49:48 +0200

    - Leagues endpoints

--- src/leagues.ts
+++ src/leagues.ts
@@ -3,11 +3,6 @@ import prisma from "./prisma-client.js";
 
 const router = Router();
 
-// GET /usuarios/
-// GET /usuarios/:id
-// POST /usuario/
-// PUT /usuario/:id
-// DELETE /usuario/:id
 
 // GET /usuario/entrenamientos
 
@@ -16,25 +11,64 @@ router.get("/", async (req, res) => {
     const leagues = await prisma.league.findMany({});
     res.status(200).json(leagues);
   } catch (e) {
-    res.status(500).send({ type: e.constructor.name, message: e.toString() });
+    res.status(500).json({ type: e.constructor.name, message: e.toString() });
   }
 });
+
+router.post("/", async (req,res) => {
+  try{
+    const newLeague = await prisma.league.create({
+      data: req.body,
+    });
+    res.status(200).json(newLeague);
+  }
+  catch (e){
+    res.status(500).json({ type: e.constructor.name, message: e.toString() });
+  }
+})
 
 router.get("/:id", async (req, res) => {
   const { id } = req.params;
   try {
-    const leagues = await prisma.league.findUnique({
+    const league = await prisma.league.findUnique({
       where: {
         id: Number(id),
       },
     });
-    if (leagues === null) {
-        res.status(404).json({error: `Liga de id:${id} no ha sido encontrada!`})
+    if (league === null) {
+      res
+        .status(404)
+        .json({ error: `Liga de id:${id} no ha sido encontrada!` });
     }
-    res.status(200).json(leagues);
+    res.status(200).json(league);
   } catch (e) {
-    res.status(500).send({ type: e.constructor.name, message: e.toString() });
+    res.status(500).json({ type: e.constructor.name, message: e.toString() });
   }
 });
 
+router.put("/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const updatedLeague = await prisma.league.update({
+      where: { id: Number(id) },
+      data: req.body,
+    });
+    res.status(200).json(updatedLeague);
+  } catch (e) {
+    res.status(500).json({ type: e.constructor.name, message: e.toString() });
+  }
+});
+
+router.delete("/:id", async (req, res)=>{
+  const {id} = req.params;
+  try{
+    const deletedLeague = await prisma.league.delete({
+      where: { id: Number(id) },
+    });
+    res.status(200).json(deletedLeague);
+  }
+  catch (e) {
+    res.status(500).json({ type: e.constructor.name, message: e.toString() });
+  }
+})
 export default router;
