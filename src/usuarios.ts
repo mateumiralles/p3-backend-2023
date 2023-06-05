8d2d0f10ce1fff37ff3f74bf9b3c3cb8ca8b997b
MateuMiralles <mateumiralles714@gmail.com>
2023-06-05 04:19:34 +0200

    - Endpoint Get One Usuario

--- src/usuarios.ts
+++ src/usuarios.ts
@@ -1,7 +1,7 @@
-import {Router} from'express';
-import prisma from './prisma-client.js';
+import { Router } from "express";
+import prisma from "./prisma-client.js";
 
-const router = Router(); 
+const router = Router();
 
 // GET /usuarios/
 // GET /usuarios/:id
@@ -12,12 +12,29 @@ const router = Router();
 // GET /usuario/entrenamientos
 
 router.get("/", async (req, res) => {
-    try {
-      const result = await prisma.usuario.findMany({});
-      res.status(200).json(result);
-    } catch (e) {
-      res.status(500).send({ type: e.constructor.name, message: e.toString() });
-    }
-  });
+  try {
+    const result = await prisma.usuario.findMany({});
+    res.status(200).json(result);
+  } catch (e) {
+    res.status(500).send({ type: e.constructor.name, message: e.toString() });
+  }
+});
 
-  export default router;
+router.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const usuario = await prisma.usuario.findUnique({
+      where: {
+        id: Number(id),
+      },
+    });
+    if (usuario === null) {
+        res.status(404).json({error: `Usuario de id:${id} no ha sido encontrado!`})
+    }
+    res.status(200).json(usuario);
+  } catch (e) {
+    res.status(500).send({ type: e.constructor.name, message: e.toString() });
+  }
+});
+
+export default router;
